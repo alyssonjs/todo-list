62890c9e5b6313423b7daa24477b22883cb2a224
alysson-santos-pontomais <alysson.santos@pontomais.com.br>
2022-01-27 16:40:49 -0300

    create sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+    def create 
+        user = User.find_by(email:login_params[:email].downcase)
+        if user && user.authenticate(login_params[:password])
+            session[:user_id] = user.id
+
+            redirect_to '/collaborator'
+        else
+            flash[:notice] = 'Algo está errado, confira seu email ou senha.'
+            redirect_to login_path
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:notice] = 'Você está deslogado'
+        redirect_to login_path
+    end
+
+    private 
+
+    def login_params
+        params.require(:login).permit(:email, :password)
+    end
+end
